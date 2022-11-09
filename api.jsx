3072ada5d818f9666c5c955035b1797917bc0ae5
Romulo <romulo.gomes.mendes@hotmail.com>
2022-11-08 22:20:18 -0300

    some settings

---
+++ api.jsx
@@ -0,0 +1,10 @@
+export const API_URL = "http://localhost:3000/";
+
+export function LOGIN_GET(body){
+    return{
+        url: API_URL + "/login",
+        options: {
+            method
+        }
+    }
+}
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactComponent as Logo } from "../../assets/login/Logo.svg";
+
+const Login = () => {
+	return (
+		<div>
+			<Logo />
+		</div>
+	);
+};
+
+export default Login;
